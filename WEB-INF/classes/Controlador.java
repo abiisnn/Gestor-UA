36e5f52fd5c9082b1afa104eb5f16e0343ea9cd1
Enrike Ramos <39656338+brokenerk@users.noreply.github.com>
2018-08-25 17:08:26 -0500

    Metodos init() y destroy() en servlets
    Para garantizar la conexion a la BD sin errores con Tomcat

--- WEB-INF/classes/Controlador.java
+++ WEB-INF/classes/Controlador.java
@@ -1,11 +1,31 @@
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.util.*;
 
 //Compilar: javac -cp ,;../lib/mysqlcon.jar;../lib/servlet-api.jar; Controlador.java
 
 public class Controlador extends HttpServlet{
 	ConexionBase conex=new ConexionBase();
+
+	//Este método se ejecuta una única vez, al ser inicializado por primera vez
+	//el servlet.Se suelen inicializar variables y ejecutar operaciones costosas
+	//en tiempo de ejecución (abrir ficheros, conectar con bases de datos, etc)
+	public void init (ServletConfig config) throws ServletException {
+		// Llamada al método init() de la superclase (GenericServlet)
+		// Así se asegura una correcta inicialización del servlet
+		super.init(config);
+		conex.establecerConexion();
+	}// fin del método init()
+
+
+	// Este método es llamado por el servidor web al "apagarse"
+	// (al hacer shut down). Sirve para proporcionar una correcta
+	// desconexión de una base de datos, cerrar ficheros abiertos, etc.
+	public void destroy () {
+		super.destroy();
+		conex.cerrarConexion();
+	} // fin de destroy()
 	
 	public String insertarQuery(String table, String[] values){//Construye el query para insertar registros
 		return "insert into "+table+" values("+values[0]+", '"+values[1]+"','"+values[2]+"')";
@@ -15,6 +35,7 @@ public class Controlador extends HttpServlet{
 		return "delete from "+table+" where clave="+id;
 	}
 
+	// Método llamada mediante un HTTP POST
 	public void doPost(HttpServletRequest peticion, HttpServletResponse respuesta)  throws ServletException, IOException{
 		String[] datos=new String[3];
 		respuesta.setContentType("text/html");
@@ -25,25 +46,8 @@ public class Controlador extends HttpServlet{
 			datos[1]=peticion.getParameter("tipo");
 			datos[2]=peticion.getParameter("bibliografia");
 
-			conex.establecerConexion();
-			conex.operacionSQL(insertarQuery("bibliografia", datos));
-			conex.cerrarConexion();
-
-	
-			/*salida.print("<HTML>");
-			salida.print("	<HEAD>");
-			salida.print("		<TITLE>");
-			salida.print("Interfaz");
-			salida.print("		</TITLE>");
-			salida.print("	</HEAD>");
-			salida.print("	<BODY><center>");
-			salida.print(	"		<P>");
-
-			salida.print(	"		</P>");
-			salida.print("	</center></BODY>");
 			
-			salida.print("</HTML>");*/
-
+			conex.operacionSQL(insertarQuery("bibliografia", datos));
 		}
 		catch(Exception ex){salida.print("ERROR"+ex);}
 		finally{salida.close();}
--- WEB-INF/classes/Interfaz.java
+++ WEB-INF/classes/Interfaz.java
@@ -3,6 +3,26 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class Interfaz extends HttpServlet{
+	ConexionBase conex=new ConexionBase();
+	//Este método se ejecuta una única vez, al ser inicializado por primera vez
+	//el servlet.Se suelen inicializar variables y ejecutar operaciones costosas
+	//en tiempo de ejecución (abrir ficheros, conectar con bases de datos, etc)
+	public void init (ServletConfig config) throws ServletException {
+		// Llamada al método init() de la superclase (GenericServlet)
+		// Así se asegura una correcta inicialización del servlet
+		super.init(config);
+		conex.establecerConexion();
+	}// fin del método init()
+
+	// Este método es llamado por el servidor web al "apagarse"
+	// (al hacer shut down). Sirve para proporcionar una correcta
+	// desconexión de una base de datos, cerrar ficheros abiertos, etc.
+	public void destroy () {
+		super.destroy();
+		conex.cerrarConexion();
+	} // fin de destroy()
+
+	// Método llamada mediante un HTTP POST
 	public void doPost(HttpServletRequest peticion, HttpServletResponse respuesta)  throws ServletException, IOException{
 		respuesta.setContentType("text/html");
 		PrintWriter salida=respuesta.getWriter();

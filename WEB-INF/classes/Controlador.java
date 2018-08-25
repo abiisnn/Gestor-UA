60558e029a67fe2931b22e562871e06341d9cc2c
Abii Snn <33185677+abiisnn@users.noreply.github.com>
2018-08-25 17:56:57 -0500

    Doc....

--- WEB-INF/classes/Controlador.java
+++ WEB-INF/classes/Controlador.java
@@ -5,28 +5,29 @@ import java.util.*;
 
 //Compilar: javac -cp ,;../lib/mysqlcon.jar;../lib/servlet-api.jar; Controlador.java
 
-public class Controlador extends HttpServlet{
-	ConexionBase conex=new ConexionBase();
-
+public class Controlador extends HttpServlet
+{	
+	ConexionBase conex = new ConexionBase();
 	/*
 		Este método se ejecuta una única vez, al ser inicializado por primera vez 
 		el servlet.Se suelen inicializar variables y ejecutar operaciones costosas
 		en tiempo de ejecución (abrir ficheros, conectar con bases de datos, etc)
 	*/
-	public void init (ServletConfig config) throws ServletException {
+	public void init (ServletConfig config) throws ServletException 
+	{
 		// Llamada al método init() de la superclase (GenericServlet)
 		// Así se asegura una correcta inicialización del servlet
 		super.init(config);
 		conex.establecerConexion();
 	} // Fin del método init()
 
-
 	/* 
 		Este método es llamado por el servidor web al "apagarse"
 		(al hacer shut down). Sirve para proporcionar una correcta
 		desconexión de una base de datos, cerrar ficheros abiertos, etc.
 	*/
-	public void destroy () {
+	public void destroy () 
+	{
 		super.destroy();
 		conex.cerrarConexion();
 	} // Fin de destroy()
@@ -44,22 +45,19 @@ public class Controlador extends HttpServlet{
 	}
 
 	// Método llamada mediante un HTTP POST
-	public void doPost(HttpServletRequest peticion, HttpServletResponse respuesta)  throws ServletException, IOException{
+	public void doPost(HttpServletRequest peticion, HttpServletResponse respuesta)  throws ServletException, IOException
+	{
 		String[] datos=new String[3];
 		respuesta.setContentType("text/html");
 		PrintWriter salida=respuesta.getWriter();
 		try{
-
 			datos[0]=peticion.getParameter("clave");
 			datos[1]=peticion.getParameter("tipo");
 			datos[2]=peticion.getParameter("bibliografia");
 
-			
 			conex.operacionSQL(insertarQuery("bibliografia", datos));
 		}
 		catch(Exception ex){salida.print("ERROR"+ex);}
 		finally{salida.close();}
 	}
-
-
 }
